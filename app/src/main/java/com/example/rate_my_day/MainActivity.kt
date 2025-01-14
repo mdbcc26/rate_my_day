7f62e6b74d1c1f0bfab190cd9e936a83717c0093
cc231050 <cc231050@fhstp.ac.at>
2025-01-14 15:49:27 +0100

    datastore preferences

--- app/src/main/java/com/example/rate_my_day/MainActivity.kt
+++ app/src/main/java/com/example/rate_my_day/MainActivity.kt
@@ -1,30 +1,42 @@
 package com.example.rate_my_day
 
+import android.content.Context
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
 import com.example.rate_my_day.data.RateDayRepository
 import com.example.rate_my_day.data.db.RateDaysDatabase
 import com.example.rate_my_day.ui.RateMyDayViewModel
 import com.example.rate_my_day.ui.RateMyDayApp
 import com.example.rate_my_day.ui.theme.RateMyDayTheme
+import kotlinx.coroutines.flow.Flow
+
 
 class MainActivity : ComponentActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) // Force Light mode
 
         val database = RateDaysDatabase.getDatabase(this)
         val repository = RateDayRepository(database.RateDaysDao())
         val viewModel = RateMyDayViewModel(repository)
 
+        val preferences by lazy {
+            com.example.rate_my_day.data.Preferences(this)
+        }
+
         enableEdgeToEdge()
         setContent {
             RateMyDayTheme {
-                    RateMyDayApp(viewModel = viewModel)
+                    RateMyDayApp(viewModel = viewModel, preferences)
             }
         }
     }
