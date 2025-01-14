86e80f04c14cc8cc988a8db780bbfcb7f81ecb6d
Matt Dalton <146341808+mdbcc26@users.noreply.github.com>
2025-01-14 15:37:32 +0100

    Delete Factory file

--- app/src/main/java/com/example/rate_my_day/ui/ViewModelFactoryProvider.kt
+++
@@ -1,9 +0,0 @@
-package com.example.rate_my_day.ui
-
-import androidx.lifecycle.viewmodel.viewModelFactory
-
-object ViewModelFactoryProvider {
-    val Factory = viewModelFactory {
-        //initializer { }
-    }
-}
