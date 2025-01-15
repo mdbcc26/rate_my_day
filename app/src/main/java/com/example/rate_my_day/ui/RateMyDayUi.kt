76491cfc1ae52f62e6078ada5aaccacb45a24700
Matt D <146341808+mdbcc26@users.noreply.github.com>
2025-01-15 21:54:42 +0100

    Reverted the passing of variables to Rate screen

--- app/src/main/java/com/example/rate_my_day/ui/RateMyDayUi.kt
+++ app/src/main/java/com/example/rate_my_day/ui/RateMyDayUi.kt
@@ -57,22 +57,6 @@ fun RateMyDayApp(
     val navController = rememberNavController()
     val backgroundColor = Color(0xFFE9D6B8)
 
-
-    // Collect ratedDays in a @Composable context
-    val ratedDays by viewModel.ratedDays.collectAsState(emptyList())
-
-    // Determine the initial route based on today's data
-    val today = LocalDate.now()
-    val todayInMillis = today.atStartOfDay().toEpochSecond(ZoneOffset.UTC) * 1000
-    val todayRateDay = ratedDays.firstOrNull { it.date == todayInMillis }
-
-    // Build the start destination dynamically
-    val startDestination = if (todayRateDay != null) {
-        "${Screens.Rate.name}/${today.toString()}/${todayRateDay.stars}/${todayRateDay.comment ?: ""}"
-    } else {
-        Screens.Rate.name
-    }
-
     Scaffold(
         modifier = Modifier
             .fillMaxSize()
@@ -82,7 +66,7 @@ fun RateMyDayApp(
         innerPadding ->
         NavHost(
             navController,
-            startDestination = startDestination,
+            startDestination = Screens.Rate.name, //startDestination,
             modifier = Modifier.padding(innerPadding)
         ) {
             composable(Screens.View.name) {
