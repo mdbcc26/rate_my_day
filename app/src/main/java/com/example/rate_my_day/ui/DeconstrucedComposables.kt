f05f08a5925dd33fd08678514980494810a389fa
Matt D <146341808+mdbcc26@users.noreply.github.com>
2025-01-15 18:55:47 +0100

    Doc. Commented out warnings

--- app/src/main/java/com/example/rate_my_day/ui/DeconstrucedComposables.kt
+++ app/src/main/java/com/example/rate_my_day/ui/DeconstrucedComposables.kt
@@ -8,18 +8,17 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxSize
+//import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+//import androidx.compose.foundation.lazy.LazyColumn
+//import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Build
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Star
@@ -32,8 +31,8 @@ import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+//import androidx.compose.runtime.collectAsState
+//import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -234,16 +233,16 @@ fun DayOptionsDialog(
         } else null
     )
 }
-
+/*
 @Composable
 fun ViewStars(
     rating: Int,
     modifier: Modifier
 ) {
 
-}
+}*/
 
-// Used for database testing and visibility of RatedDays table.
+/** Used for database testing and visibility of RatedDays table.
 @Composable
 fun ReadTableRatedDays(viewModel: RateMyDayViewModel) {
     val ratedDays by viewModel.ratedDays.collectAsState()
@@ -262,6 +261,7 @@ fun ReadTableRatedDays(viewModel: RateMyDayViewModel) {
         }
     }
 }
+*/
 
 @Composable
 fun RatingLegend() {
